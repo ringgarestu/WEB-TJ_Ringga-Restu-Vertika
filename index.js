dd039b30aa7d9f950768043c042c45d26ab7aeed
ringga vertika <ringgarestu@gmail.com>
2023-04-19 15:53:33 +0700

    push-pertama

---
+++ index.js
@@ -0,0 +1,13 @@
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true,
+    },
+});
